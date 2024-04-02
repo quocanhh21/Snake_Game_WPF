05143e818519abd47991c14db89df3d0a67de942
anh.le <anh.le@s3corp.com.vn>
2024-04-02 17:51:56 +0700

    Add project files.

---
+++ Direction.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Snake
+{
+    public class Direction
+    {
+        public readonly static Direction Left = new Direction(0 ,-1);
+        public readonly static Direction Right = new Direction(0 ,+1);
+        public readonly static Direction Up = new Direction(-1 ,0);
+        public readonly static Direction Down = new Direction(+1 ,0);
+        public int RowOffSet { get; }
+        public int ColumnOffSet { get; }
+
+        public Direction(int rowOffSet, int columnOffSet)
+        {
+            RowOffSet = rowOffSet;
+            ColumnOffSet = columnOffSet;
+        }
+
+        public Direction Opposite()
+        {
+            return new Direction(-RowOffSet, -ColumnOffSet);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Direction direction &&
+                   RowOffSet == direction.RowOffSet &&
+                   ColumnOffSet == direction.ColumnOffSet;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RowOffSet, ColumnOffSet);
+        }
+
+        public static bool operator ==(Direction? left, Direction? right)
+        {
+            return EqualityComparer<Direction>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Direction? left, Direction? right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ GameState.cs
@@ -0,0 +1,169 @@
+﻿namespace Snake
+{
+    public class GameState
+    {
+        public int Rows { get; }
+        public int Columns { get; }
+        public GridValue[,] Grid { get; set; }
+        public Direction Direction { get; private set; }
+        public int Score { get; set; }
+        public bool GameOver { get; set; }
+
+        private readonly LinkedList<Direction> dirChanges = new LinkedList<Direction>();
+        private readonly LinkedList<Position> SnakePositions = new LinkedList<Position>();
+        public readonly Random Random = new Random();
+
+        public GameState(int rows, int cols)
+        {
+            Rows = rows;
+            Columns = cols;
+            Grid = new GridValue[Rows, Columns];
+            Direction = Direction.Right;
+
+            AddSnake();
+            AddFood();
+        }
+
+        private void AddSnake()
+        {
+            int r = Rows / 2;
+
+            for (int c = 0; c < 3; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                SnakePositions.AddFirst(new Position(r, c));
+            }
+        }
+
+        private IEnumerable<Position> EmptyPosition()
+        {
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Columns; c++)
+                {
+                    if (Grid[r, c] == GridValue.Empty)
+                    {
+                        yield return new Position(r, c);
+                    }
+                }
+            }
+        }
+
+        private void AddFood()
+        {
+            List<Position> empty = new List<Position>(EmptyPosition());
+            if (empty.Count == 0)
+                return;
+
+            Position position = empty[Random.Next(empty.Count)];
+            Grid[position.Row, position.Column] = GridValue.Food;
+        }
+
+        public Position HeadPosition()
+        {
+            return SnakePositions.First.Value;
+        }
+
+        public Position TailPosition()
+        {
+            return SnakePositions.Last.Value;
+        }
+
+        public IEnumerable<Position> ListSnakePositions()
+        {
+            return SnakePositions;
+        }
+
+        private void AddHead(Position position)
+        {
+            SnakePositions.AddFirst(position);
+            Grid[position.Row, position.Column] = GridValue.Snake;
+        }
+
+        private void RemoveTail()
+        {
+            Position tail = SnakePositions.Last.Value;
+            Grid[tail.Row, tail.Column] = GridValue.Empty;
+            SnakePositions.RemoveLast();
+        }
+
+        private Direction GetLastDirection()
+        {
+            if (dirChanges.Count == 0)
+            {
+                return Direction;
+            }
+
+            return dirChanges.Last.Value;
+        }
+
+        private bool CanChangeDirection(Direction newDir)
+        {
+            if (dirChanges.Count == 2)
+            {
+                return false;
+            }
+
+            Direction lastDir = GetLastDirection();
+            return newDir != lastDir && newDir != lastDir.Opposite();
+        }
+
+        public void ChangeDirection(Direction direction)
+        {
+            // if can change direction
+            if (CanChangeDirection(direction))
+            {
+                dirChanges.AddLast(direction);
+            }
+        }
+
+        private bool OutsideGrid(Position position)
+        {
+            return position.Row < 0 || position.Row >= Rows || position.Column < 0 || position.Column >= Columns;
+        }
+
+        private GridValue WillHit(Position newHeadPosition)
+        {
+            if (OutsideGrid(newHeadPosition))
+            {
+                return GridValue.Outside;
+            }
+
+            // Special case: New head == old tail
+            if (newHeadPosition == TailPosition())
+            {
+                return GridValue.Empty;
+            }
+
+            return Grid[newHeadPosition.Row, newHeadPosition.Column];
+        }
+
+        public void Move()
+        {
+            if (dirChanges.Count>0)
+            {
+                Direction = dirChanges.First.Value;
+                dirChanges.RemoveFirst();
+            }
+
+            Position newHeadPosition = HeadPosition().Translate(Direction);
+            GridValue hit = WillHit(newHeadPosition);
+
+            if (hit == GridValue.Outside || hit == GridValue.Snake)
+            {
+                GameOver = true;
+            }
+            else if (hit == GridValue.Empty)
+            {
+                RemoveTail();
+                AddHead(newHeadPosition);
+            }
+            else if (hit == GridValue.Food)
+            {
+                AddHead(newHeadPosition);
+                Score++;
+                AddFood();
+            }
+        }
+    }
+}
